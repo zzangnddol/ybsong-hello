6bcdc32f54dcdaca79e1d5d4cbb2a58c1846a155
송영배 <youngbae.song@ufinite.com>
2013-05-02 03:53:28 +0900

    수정

--- src/main/java/com/ybsong/util/hello/Hello.java
+++ src/main/java/com/ybsong/util/hello/Hello.java
@@ -2,6 +2,10 @@ package com.ybsong.util.hello;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		new Hello().sayHello("Youngbae");
+	}
+
+	public void sayHello(String name) {
+		System.out.println("Hello, " + name + "!!!");
 	}
 }
